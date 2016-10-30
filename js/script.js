09541243acaccf8fb2671f92b30dec17786cfa59
Jeanmarie Vargas <jeanmarie.vargas@gmail.com>
2016-10-30 18:42:32 -0400

    added p5 sound

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(window).ready(function(){
    // audio.play();
     var ctx = new AudioContext();
     var analyser = ctx.createAnalyser();
-    console.log("audio: " + audio.currentSrc + " ready state: " + audio.readyState);
+   // console.log("audio: " + audio.currentSrc + " ready state: " + audio.readyState);
     var audioSource = ctx.createMediaElementSource(audio);
     audioSource.connect(analyser);
     audioSource.connect(ctx.destination);
@@ -22,14 +22,30 @@ $(window).ready(function(){
         analyser.getByteFrequencyData(frequencyData);
      //   console.log(frequencyData); // now you can read the frequency data!
         console.log("length: " + frequencyData.length);
-        for(bin = 0; bin < frequencyData.length; bin++) {
+       /* for(bin = 0; bin < frequencyData.length; bin++) {
             var val = frequencyData[bin];
+            //            var val = 100
+
             var red = 255-val;
             var green = val/2;
             var blue = val;
             context.fillStyle = 'rgb('+red+', '+green+', '+blue+')';
             console.log(red+ " " + green + " " + blue);
-            context.fillRect(bin * 2,0,2,200);
+           // context.fillRect(bin * 2,0,2,200);
+          //  context.fillRect(0,10,1000,10);
+         //   context.fillRect(bin * 2, 0, bin, 10);
+              context.fillRect(bin*2, 0, bin, 10);
+
+
+        }*/
+        var barx, barwidth, barheight;
+        context.fillStyle = '#00CCFF';
+        bars = 100;
+        for(var i = 0; i < bars; i++) {
+            barx = i * 3;
+            barwidth = 2;
+            barheight = (frequencyData[i]/2);
+            context.fillRect(barx, canvas.height, barwidth, barheight);
         }
         requestAnimationFrame(renderFrame);
     }
